a2f5f6a8118a80a91b8d1831e3232913f6f78191
Cartoon-World-97 <cartoonworld.server@gmail.com>
2026-01-21 23:41:12 +0530

    Create PrivacyPolicy.jsx

---
+++ src/Pages/PrivacyPolicy.jsx
@@ -0,0 +1,21 @@
+function PrivacyPolicy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+        Cartoon World Web App uses Google AdSense to display ads. Google may
+        use cookies to show ads based on your visits to this site and other
+        websites.
+      </p>
+      <p>
+        All videos are embedded from YouTube via official iframe. We do not
+        host or upload any videos ourselves.
+      </p>
+      <p>
+        Users can opt out of personalized ads by visiting Google Ads Settings.
+      </p>
+    </div>
+  );
+}
+
+export default PrivacyPolicy;
